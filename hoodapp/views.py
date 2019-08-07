bcb80a439597ecb1613ee33b753a5bb4fdb73e8f
williammango <williammango2015@gmail.com>
2019-08-07 17:22:44 +0300

    created templeate and registration sub template ang authentication templates

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login')
+    else:
+        form =UserRegistrationForm()
+    return render(request,'registration/registration_form.html',{'form':form}) 
