34e22cd453b46e9329ef0d859bbf22468668c9c1
williammango <williammango2015@gmail.com>
2019-08-08 14:39:48 +0300

    made migrations and created the admin dash board

--- hood/settings.py
+++ hood/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'mango',
+    'PASSWORD':'mango',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,10 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class UserS(models.Model):
+
+class Hood(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    count = models.IntegerField()
+    admin = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    
+    
+    
+class Business(models.Model):
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, null=True, blank=True )
+    hood = models.ForeignKey(Hood, null=True, blank=True)
+    email = models.EmailField()
+
+class User(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField()
-  
+    id = models.IntegerField(primary_key=True)
+    hood = models.ForeignKey(User, null=True, blank=True)
+    
     def __str__(self):
             return self.name
+class Profile(models.Model):
+    image = models.ImageField(default = 'default.jpg',upload_to='images/')
+    bio = models.CharField(max_length=150)
+    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True, related_name='profile') 
+    
+    
+    def __str__(self):
+            return self.user.username
+    class Meta:
+        ordering =['bio']
+    
+    def save_profile(self):
+        self.save()
+    def update_profile(self):
+        self.update()
+    def delete_profile(self):
+        self.delete()
