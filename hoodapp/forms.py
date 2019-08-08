880855376c64dc8bee5f3d393e37b68aab078f82
williammango <williammango2015@gmail.com>
2019-08-08 16:28:11 +0300

    modified forms by add user,profile,and hood creating forms

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Hood,Profile,Business
 
 class UserRegistrationForm(UserCreationForm):
     email =forms.EmailField()
@@ -8,3 +9,20 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+
+class hoodForm(forms.ModelForm):
+    class Meta:
+        model = Hood
+        exclude = ['admin']
+        
+class userForm(forms.ModelForm):
+    email =forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class profileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']
